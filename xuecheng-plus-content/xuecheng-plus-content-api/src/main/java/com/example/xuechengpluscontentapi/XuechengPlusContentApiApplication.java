191df1f55f3ea413b7bc6d3f47a87710e59b3bb1
zs0112 <1958929104@qq.com>
2023-03-10 18:31:13 +0800

    基础

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/example/xuechengpluscontentapi/XuechengPlusContentApiApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/example/xuechengpluscontentapi/XuechengPlusContentApiApplication.java
@@ -3,6 +3,9 @@ package com.example.xuechengpluscontentapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @deprecated 内容管理服务启动类
+ */
 @SpringBootApplication
 public class XuechengPlusContentApiApplication {
 
